37282e3bfa675548a0ea58531a00a33bc8a34f9c
simplyratl <nikicaraznatovic123@gmail.com>
2022-10-17 21:38:35 +0200

    netlify routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import "./style/style.scss";
 import Game from "./pages/Game";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import {useAuthState} from "react-firebase-hooks/auth";
-import {auth} from "./firebase";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
 import UserForm from "./components/Login/UserForm";
 import GuestHome from "./components/Login/GuestHome";
 import Navbar from "./components/Navbar";
@@ -12,41 +12,34 @@ import Register from "./pages/Authentication/Register";
 import AuthHome from "./pages/AuthHome";
 
 function App() {
-    const [user, loading, error] = useAuthState(auth);
+  const [user, loading, error] = useAuthState(auth);
 
-    if (loading) {
-        return (
-          <div className="h-[100vh]">
-              <h2>
-                  <Loader position="absolute" center={true}/>
-              </h2>
-          </div>
-        );
-    }
-    if (error) {
-        return (
-          <>
-              <h2>There was an error ...</h2>
-          </>
-        );
-    }
-
+  if (loading) {
     return (
-      <main className="min-h-[100vh] overflow-x-hidden">
-          <Router>
-              <Navbar/>
-              <Routes>
-                  <Route path="/guest" element={<GuestHome/>}/>
-                  <Route path="/" element={<AuthHome/>}/>
-                  <Route path="/login-guest" element={<UserForm/>}/>
-                  <Route path="/login" element={<Login/>}/>
-                  <Route path="/register" element={<Register/>}/>
-                  <Route path="/game/:id" element={<Game/>}/>
-                  <Route path="/game/offline" element={<Game/>}/>
-              </Routes>
-          </Router>
-      </main>
+      <div className="h-[100vh]">
+        <h2>
+          <Loader position="absolute" center={true} />
+        </h2>
+      </div>
     );
+  }
+
+  return (
+    <main className="min-h-[100vh] overflow-x-hidden">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/guest" element={<GuestHome />} />
+          <Route path="/" element={<AuthHome />} />
+          <Route path="/login-guest" element={<UserForm />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/game/:id" element={<Game />} />
+          <Route path="/game/offline" element={<Game />} />
+        </Routes>
+      </Router>
+    </main>
+  );
 }
 
 export default App;
